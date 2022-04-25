19015c48c23ec197aae9ffa3dcfedfc9b69569a0
AndrisJa <100909331+AndrisJa@users.noreply.github.com>
2022-04-25 13:44:11 +0300

    logaritms

--- kalkulators.py
+++ kalkulators.py
@@ -1,5 +1,7 @@
 from argparse import MetavarTypeHelpFormatter
+from ast import operator
 from tkinter import*
+from math import*
 mansLogs=Tk()
 mansLogs.title("Kalkulators")
 #mansLogs.geometry("300x400")
@@ -16,31 +18,67 @@ def vienads():
         result=num1*num2
     elif mathOp=="/":
         result=num1/num2
+    else:
+        result=0
+    e.delete(0,END)
+    e.insert(0,str(result))
     return 0
 
 def notirit():
-    e.delet(0, END)
+    e.delete(0, END)
     num1=0
     mathop=""
     return 0
 
+def sq_rt():
+    global operator
+    global num1
+    global mathOp
+    num1=(float(e.get()))
+    num1=sqrt(num1)
+    e.delete(0,END)
+    e.insert(0,num1)
+    return 0
 
-btn0 = Button(mansLogs, text="0", padx="40", pady="20", command=lambda:btnClick(0),bg="grey", fg="blue")
-btn1 = Button(mansLogs, text="1", padx="40", pady="20", command=lambda:btnClick(1),bg="grey", fg="blue")
-btn2 = Button(mansLogs, text="2", padx="40", pady="20", command=lambda:btnClick(2),bg="grey", fg="blue")
-btn3 = Button(mansLogs, text="3", padx="40", pady="20", command=lambda:btnClick(3),bg="grey", fg="blue")
-btn4 = Button(mansLogs, text="4", padx="40", pady="20", command=lambda:btnClick(4),bg="grey", fg="blue")
-btn5 = Button(mansLogs, text="5", padx="40", pady="20", command=lambda:btnClick(5),bg="grey", fg="blue")
-btn6 = Button(mansLogs, text="6", padx="40", pady="20", command=lambda:btnClick(6),bg="grey", fg="blue")
-btn7 = Button(mansLogs, text="7", padx="40", pady="20", command=lambda:btnClick(7),bg="grey", fg="blue")
-btn8 = Button(mansLogs, text="8", padx="40", pady="20", command=lambda:btnClick(8),bg="grey", fg="blue")
-btn9 = Button(mansLogs, text="9", padx="40", pady="20", command=lambda:btnClick(9),bg="grey", fg="blue")
-btn10 = Button(mansLogs, text="+", padx="40", pady="20", command=lambda:btnClick("+"),bg="grey", fg="white")
-btn11 = Button(mansLogs, text="-", padx="40", pady="20", command=lambda:btnClick("-"),bg="grey", fg="white")
-btn12 = Button(mansLogs, text="/", padx="40", pady="20", command=lambda:btnClick("/"),bg="grey", fg="white")
-btn13 = Button(mansLogs, text="*", padx="40", pady="20", command=lambda:btnClick("*"),bg="grey", fg="white")
-btn14 = Button(mansLogs, text="=", padx="40", pady="20", command=vienads, bg="grey", fg="white")
-btn15 = Button(mansLogs, text="C", padx="40", pady="20", command=notirit, bg="grey", fg="red")
+def kvadrats():
+    global operator
+    global num1
+    global mathOp
+    num1=(float(e.get()))
+    num1=num1**2
+    e.delete(0,END)
+    e.insert(0,num1)
+    return 0
+
+def logaritms():
+    global operator
+    global num1
+    global mathOp
+    num1=(float(e.get()))
+    num1=log(num1, 10)
+    e.delete(0,END)
+    e.insert(0,num1)
+    return 0
+
+btn0 = Button(mansLogs, text="0", padx="40", pady="20", command=lambda:btnClick(0),bg="grey", fg="white")
+btn1 = Button(mansLogs, text="1", padx="40", pady="20", command=lambda:btnClick(1),bg="grey", fg="white")
+btn2 = Button(mansLogs, text="2", padx="40", pady="20", command=lambda:btnClick(2),bg="grey", fg="white")
+btn3 = Button(mansLogs, text="3", padx="40", pady="20", command=lambda:btnClick(3),bg="grey", fg="white")
+btn4 = Button(mansLogs, text="4", padx="40", pady="20", command=lambda:btnClick(4),bg="grey", fg="white")
+btn5 = Button(mansLogs, text="5", padx="40", pady="20", command=lambda:btnClick(5),bg="grey", fg="white")
+btn6 = Button(mansLogs, text="6", padx="40", pady="20", command=lambda:btnClick(6),bg="grey", fg="white")
+btn7 = Button(mansLogs, text="7", padx="40", pady="20", command=lambda:btnClick(7),bg="grey", fg="white")
+btn8 = Button(mansLogs, text="8", padx="40", pady="20", command=lambda:btnClick(8),bg="grey", fg="white")
+btn9 = Button(mansLogs, text="9", padx="40", pady="20", command=lambda:btnClick(9),bg="grey", fg="white")
+btn10 = Button(mansLogs, text="+", padx="40", pady="20", command=lambda:btnCommand("+"),bg="grey", fg="white")
+btn11 = Button(mansLogs, text="-", padx="40", pady="20", command=lambda:btnCommand("-"),bg="grey", fg="white")
+btn12 = Button(mansLogs, text="/", padx="40", pady="20", command=lambda:btnCommand("/"),bg="grey", fg="white")
+btn13 = Button(mansLogs, text="*", padx="40", pady="20", command=lambda:btnCommand("*"),bg="grey", fg="white")
+btn14 = Button(mansLogs, text="sqrt", padx="40", pady="20", command=sq_rt, bg="grey", fg="white")
+btn15 = Button(mansLogs, text="square", padx="40", pady="20", command=kvadrats, bg="grey", fg="white")
+btn16 = Button(mansLogs, text="log", padx="40", pady="20", command=logaritms, bg="grey", fg="white")
+btn17 = Button(mansLogs, text="=", padx="40", pady="20", command=vienads, bg="grey", fg="white")
+btn18 = Button(mansLogs, text="C", padx="40", pady="20", command=notirit, bg="grey", fg="red")
 
 btn0.grid(row=4, column=2)
 btn1.grid(row=3, column=1)  
@@ -56,23 +94,26 @@ btn10.grid(row=2, column=4)
 btn11.grid(row=1, column=4)
 btn12.grid(row=3, column=4)
 btn13.grid(row=4, column=4)
-btn14.grid(row=4, column=1)
-btn15.grid(row=4, column=3)
+btn14.grid(row=5, column=4)
+btn15.grid(row=5, column=3)
+btn16.grid(row=5, column=2)
+btn17.grid(row=4, column=1)
+btn18.grid(row=4, column=3)
 
 def btnClick(number):
     current=e.get() #nolasa esošo skaitli
-    e.delete(0, END) #nodzēš
+    e.delete(0,END) #nodzēš
     newNumber=str(current)+str(number)
-    e.insert(0, newNumber) #ievieto displejā
+    e.insert(0,newNumber) #ievieto displejā
     return 0
 
 
 def btnCommand(command):
-    global num1
-    global mathOp
-    mathOp = command
+    global num1 #jāiegaumē skatlis un darbība
+    global mathOp #matemātisks operators
+    mathOp = command #+, -, /, *
     num1 = float(e.get())
-    e.delete(0, END)
+    e.delete(0,END)
     return 0
 
 
